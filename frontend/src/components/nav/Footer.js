910e6682036164bbcbb28f03324b259723febb52
Black-Ice1104 <yijiasun@usc.edu>
2023-09-07 14:54:54 -0700

    frontend update

--- frontend/src/components/nav/Footer.js
+++ frontend/src/components/nav/Footer.js
@@ -1,11 +1,11 @@
 export default function Footer() {
     return (
       <div className="text-center p-4 bg-dark text-light mt-4">
-        <h4 className="mt-4">Real Estate Marketplace App - Buy Sell or Rent Properties</h4>
+        <h4 className="mt-4">Realist Deal - Buy Sell or Rent Properties</h4>
+        <h5 className="mt-4">Developed by Bellamy Sun</h5>
         <p className="mt-3">
-          &copy; {new Date().getFullYear()} All rights reserved
+          &copy; {new Date().getFullYear()} All rights reserved - realistdeal.online
         </p>
-        {/* <p>domain.com</p> */}
       </div>
     );
   }
